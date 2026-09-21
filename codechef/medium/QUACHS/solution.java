d0d3fca61efb64b9336e83052ea05aa236f1c70f
Animesh Nandi <nandiani76817@gmail.com>
2026-09-21 19:57:14 +0530

    ✅ QUACHS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/QUACHS/solution.java
+++ codechef/medium/QUACHS/solution.java
@@ -7,6 +7,19 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		if(scanner.hasNextInt()) {
+		    int X = scanner.nextInt();
+		    int Y = scanner.nextInt();
+		    int Z = scanner.nextInt();
+		    
+		    int totalChips = X + y;
+		    int unavailableChips = 2* Z;
+		    int remainingChips = totalChips - unavailableChips;
+		    
+		    System.out.System.out.println(remainingChips);
+		}
+		
+		scanner.close();
 
 	}
 }
